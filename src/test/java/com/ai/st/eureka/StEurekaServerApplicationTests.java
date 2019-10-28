33307332bf3314c9b18b17ec70d6550973efebab
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-10-28 10:35:25 -0500

    configuration eureka server

---
+++ src/test/java/com/ai/st/eureka/StEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.st.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
